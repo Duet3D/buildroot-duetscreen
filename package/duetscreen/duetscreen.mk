73d69d593e645d2a0c3db008c0b91e8bc812b93c
Christian Hammacher <bmasterc@gmail.com>
2025-01-29 10:57:58 +0100

    Added DuetScreen package

---
+++ package/duetscreen/duetscreen.mk
@@ -0,0 +1,33 @@
+################################################################################
+#
+# DuetScreen
+#
+################################################################################
+
+DUETSCREEN_VERSION = HEAD
+DUETSCREEN_SITE = /home/christian/Duet3D/DuetScreen
+DUETSCREEN_SITE_METHOD = local
+DUETSCREEN_LICENSE = MIT
+DUETSCREEN_LICENSE_FILES = license.txt
+DUETSCREEN_DEPENDENCIES = libusb-compat sdl2
+
+DUETSCREEN_PRESET = T113-Debug
+DUETSCREEN_CONF_OPTS = --preset $(DUETSCREEN_PRESET)
+
+# Don't use hashes during development
+BR_NO_CHECK_HASH_FOR += $(DUETSCREEN_SOURCE)
+
+# cmake-package build does not cd into the src directory unlike the configure step...
+define DUETSCREEN_BUILD_CMDS
+	cd $(@D); \
+	$(TARGET_MAKE_ENV) $(DUETSCREEN_BUILD_ENV) $(BR2_CMAKE) --build $(DUETSCREEN_BUILDDIR) --preset $(DUETSCREEN_PRESET) -j$(PARALLEL_JOBS) $(DUETSCREEN_BUILD_OPTS)
+endef
+
+define DUETSCREEN_INSTALL_TARGET_CMDS
+	install -d -m755 $(TARGET_DIR)/opt/DuetScreen
+	install -m644 $(DUETSCREEN_PKGDIR)/config.json $(TARGET_DIR)/etc/config.json
+	install -m755 $(@D)/out/build/$(DUETSCREEN_PRESET)/DuetScreen $(TARGET_DIR)/usr/bin/DuetScreen
+	install -m755 $(@D)/out/build/$(DUETSCREEN_PRESET)/lib/* $(TARGET_DIR)/usr/lib
+endef
+
+$(eval $(cmake-package))
