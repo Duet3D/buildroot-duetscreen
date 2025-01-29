35914150e2702ae6acbb5dd3112072301a69042d
Christian Hammacher <bmasterc@gmail.com>
2025-01-29 21:56:01 +0100

    Allow custom duetscreen source path

--- package/duetscreen/duetscreen.mk
+++ package/duetscreen/duetscreen.mk
@@ -5,9 +5,14 @@
 ################################################################################
 
 DUETSCREEN_VERSION = HEAD
+ifdef DUETSCREEN_SRC
+DUETSCREEN_SITE = $(DUETSCREEN_SRC)
+DUETSCREEN_SITE_METHOD = local
+else
 DUETSCREEN_SITE = git@github.com:Duet3D/duetscreen.git
 DUETSCREEN_SITE_METHOD = git
 DUETSCREEN_GIT_SUBMODULES = YES
+endif
 DUETSCREEN_LICENSE = MIT
 DUETSCREEN_LICENSE_FILES = license.txt
 DUETSCREEN_DEPENDENCIES = libusb-compat sdl2
