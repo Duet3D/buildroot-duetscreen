e41bb53a033528561ce9de8a1c794e916619070b
Christian Hammacher <chr.hammacher@gmail.com>
2025-11-13 12:02:40 +0100

    Major improvements
    Kicked out proprietary boot0 and uboot in favour of awboot
    Unified SD and SPINAND images
    Modified boot and expansion scripts for operation on NAND
    General cleanup

--- board/duet3d/duetscreen/scripts/post-build.sh
+++ board/duet3d/duetscreen/scripts/post-build.sh
@@ -1,21 +1,9 @@
 #!/bin/sh
 
-echo "=================================> ARGS = $argv"
-
-BOARD_COMMON_DIR="$(dirname $0)/../../../allwinner-generic/$4"
 BOARD_DIR="$(dirname $0)/.."
-
-# Copy Platfrom Files to BINARY_DIR
-cp -rfvd $BOARD_COMMON_DIR/bin/*  $BINARIES_DIR
-
-# Copy common file to BINARY_DIR
-cp -rfvd $BOARD_COMMON_DIR/../sunxi-generic/bin/* $BINARIES_DIR
 
 # Copy board files to BINARY_DIR
 cp -rfvd $BOARD_DIR/bin/* $BINARIES_DIR
-
-# Copy DTB
-#cp $BINARIES_DIR/../build/linux-*/arch/arm/boot/dts/$5 $BINARIES_DIR
 
 # Make boot directory
 mkdir "$TARGET_DIR/boot"
@@ -23,27 +11,4 @@ mkdir "$TARGET_DIR/boot"
 # Delete unneeded files
 rm -f "$TARGET_DIR/etc/init.d/S50dropbear"
 rm -f "$TARGET_DIR/lib/dhcpcd/dhcpcd-hooks/50-timesyncd.conf"
-
-# Make boot package
-cd $BINARIES_DIR
-echo "item=dtb, $5" >> boot_package.cfg
-$BINARIES_DIR/dragonsecboot -pack boot_package.cfg
-
-# Make boot bundle for SPINAND
-dd if=/dev/zero of=boot-bundle.img bs=1024 count=1152
-dd if=boot0_nand.fex of=boot-bundle.img conv=notrunc
-dd if=boot_package.fex of=boot-bundle.img seek=40 conv=notrunc
-# TODO: Add env/env-backup to seek=1136 + 1144
-
-# Make uImage
-mkimage -A arm -O linux -T kernel -C none -a 0x40008000 -n "Linux kernel" -d zImage uImage
-
-# Make uboot env
-#$BINARIES_DIR/mkenvimage -r -p 0x00 -s 131072 -o env.fex env.cfg
-
-# Make bootable image (with pseudo-ramdisk)
-#mkbootimg --kernel zImage --ramdisk ramdisk.img --board sun8iw20p1 --base 0x40200000 --kernel_offset 0x0 --ramdisk_offset 0x01000000 -o boot.img
-
-# Make bootable image (without ramdisk)
-# mkbootimg --kernel zImage --board sun8iw20p1 --base 0x40000000 -o boot.img
 
