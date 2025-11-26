c334e5671285551d06ac33186934686741165c25
Christian Hammacher <chr.hammacher@gmail.com>
2025-11-26 15:57:26 +0100

    Generate NAND update ZIP automatically
    Removed nand_factory_reset script from image and moved it to update pkg

--- board/duet3d/duetscreen/scripts/genimage.sh
+++ board/duet3d/duetscreen/scripts/genimage.sh
@@ -1,48 +1,10 @@
 #!/usr/bin/env bash
 
-die() {
-  cat <<EOF >&2
-Error: $@
+# Use default genimage script
+"${BASE_DIR}/../support/scripts/genimage.sh" $@
 
-Usage: ${0} -c GENIMAGE_CONFIG_FILE
-EOF
-  exit 1
-}
-
-# Parse arguments and put into argument list of the script
-opts="$(getopt -n "${0##*/}" -o c: -- "$@")" || exit $?
-eval set -- "$opts"
-
-GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
-
-while true ; do
-	case "$1" in
-	-c)
-	  GENIMAGE_CFG="${2}";
-	  shift 2 ;;
-	--) # Discard all non-option parameters
-	  shift 1;
-	  break ;;
-	*)
-	  die "unknown option '${1}'" ;;
-	esac
-done
-
-[ -n "${GENIMAGE_CFG}" ] || die "Missing argument"
-
-# Pass an empty rootpath. genimage makes a full copy of the given rootpath to
-# ${GENIMAGE_TMP}/root so passing TARGET_DIR would be a waste of time and disk
-# space. We don't rely on genimage to build the rootfs image, just to insert a
-# pre-built one in the disk image.
-
-trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
-ROOTPATH_TMP="$(mktemp -d)"
-
-rm -rf "${GENIMAGE_TMP}"
-
-genimage \
-	--rootpath "${ROOTPATH_TMP}"     \
-	--tmppath "${GENIMAGE_TMP}"    \
-	--inputpath "${BINARIES_DIR}"  \
-	--outputpath "${BINARIES_DIR}" \
-	--config "${GENIMAGE_CFG}"
+# Generate NAND update package
+echo -n "Generating NAND update package... "
+cd "${BINARIES_DIR}"
+tar -czf update.tar.gz awboot-boot-spi.bin sun8i-duet3d-duetscreen-linux.dtb optee.bin zImage rootfs.ubi post-update
+echo "Done!"
--- board/duet3d/duetscreen/scripts/post-build.sh
+++ board/duet3d/duetscreen/scripts/post-build.sh
@@ -11,4 +11,3 @@ mkdir "$TARGET_DIR/boot"
 # Delete unneeded files
 rm -f "$TARGET_DIR/etc/init.d/S50dropbear"
 rm -f "$TARGET_DIR/lib/dhcpcd/dhcpcd-hooks/50-timesyncd.conf"
-
