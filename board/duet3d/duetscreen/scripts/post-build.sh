b8696994e5cd79e89460364628793ced9fc44224
AndyEveritt <38423143+AndyEveritt@users.noreply.github.com>
2026-02-25 15:01:41 +0000

    feat: check update is for a compatible buildroot version

--- board/duet3d/duetscreen/scripts/post-build.sh
+++ board/duet3d/duetscreen/scripts/post-build.sh
@@ -11,3 +11,7 @@ mkdir "$TARGET_DIR/boot"
 # Delete unneeded files
 rm -f "$TARGET_DIR/etc/init.d/S50dropbear"
 rm -f "$TARGET_DIR/lib/dhcpcd/dhcpcd-hooks/50-timesyncd.conf"
+
+# Write most recent git tag to /etc/buildroot_version
+GIT_TAG=$(git -C "$(dirname $0)/../.." describe --tags --abbrev=0 2>/dev/null)
+echo "${GIT_TAG}" > "$TARGET_DIR/etc/buildroot_version"
